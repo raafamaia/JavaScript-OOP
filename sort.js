441c2068131ca3459066b60097185ba2d518c336
Maia <raafamaia@gmail.com>
2015-01-11 21:01:10 -0200

    Add comments on the sort section

--- sort.js
+++ sort.js
@@ -1,9 +1,17 @@
+/* The sort function accepts another function as the first argument, that need
+to return a negative value if the first argument is bigger then the second, a
+positive value if the second argument is bigger, and zero if they are equal
+
+Cool huh? If you don't do this shit, the default behavior of the sort() method
+is to transform everything in String and then order as strings...
+*/
+
 //------------------------------------------------------------------------------
 var numbers = [1, 3, 5, 7, 9, 2, 4, 6, 8, 0, 10, 20, 30, 33, 22, 11];
 numbers.sort(function(first, second){
 	return first - second;
 });
-//nice print
+//nice print with my function that takes 2 arguments, to compare one to another
 console.log(numbers);
 //------------------------------------------------------------------------------
 numbers.sort();
