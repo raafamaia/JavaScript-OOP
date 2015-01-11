ba7a156fa16944843899d21faf04b41b7a57ba4b
Maia <raafamaia@gmail.com>
2015-01-11 20:52:01 -0200

    Playing with 'this'

--- playing with "this".js
+++ playing with "this".js
@@ -44,7 +44,6 @@ sayName.call(person2, "Hey, I'm person1 object, my name?");
 
 console.log("\n---------------------------THE APPLY METHOD------------------------------------\n");
 
-
 /* The apply() method works exactly the same as the call() method, except that
 it accepts only two parameters: the "this" value and an array like object of
 parameters, like the arguments[] one. This way you can pass multiple parameters
@@ -74,5 +73,52 @@ sayMyName.apply(randomPerson, [randomPerson.name]);
 
 console.log("\n---------------------------THE BIND METHOD------------------------------------\n");
 
+/* This one behaves quite differently than the other two methods.
+The bind() method takes as the first argument the "this" valuefor the new 
+function, all other parameters represent named parameters that should be
+permanently set in the new function. You can still pass wharever you want later.
+*/
 
+function sayMyName(who){
+	console.log(who + " : " + this.name);
+}
 
+var person1 = {
+	name: "Little One"
+}
+
+var person2 = {
+	name: "Big One"
+}
+
+//Now I'm gonna create a function just for the Little One (person1)
+
+var sayYourNameLittleOne = sayMyName.bind(person1);
+sayYourNameLittleOne("Person1");
+
+//Gonna do the same thing for the Big One (person2)
+
+var sayYourNameBigOne = sayMyName.bind(person2, "Hey, I'm the person2 object"
++ "\n(and I just passed this message on the bind() method, see?... oh, My name is");
+sayYourNameBigOne();
+
+//Now the methods are binded with the objects, even if I attach the method with 
+//another object, the "this wont't change"
+
+var person3 = {
+	name: "Shit, I don't even have a name"
+}
+
+//First, I'm gonna use the little one method, that don't have the message
+//(second parameter) binded
+
+person3.sayName = sayYourNameLittleOne;
+person3.sayName("I'm person3 but... hey, who's talking all this shit? I'm not the Little One!");
+
+//Now with the Big One method, that the second parameter is attached
+person3.sayName = sayYourNameBigOne;
+person3.sayName("Why try? this shit will not even print.");
+
+/* See how it worked? No parameters are bound for the Little one method, so you
+still need to pass in the label for the output. The Big One method not only binds
+"this", to person2, but also binds the parameters passed, like the message */
