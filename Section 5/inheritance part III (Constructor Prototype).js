a63197b1f25808cbafb51699a78948109aa61b27
Maia <raafamaia@gmail.com>
2015-01-23 12:17:34 -0200

    Inheritance Done

--- Section 5/inheritance part III (Constructor Prototype).js
+++ Section 5/inheritance part III (Constructor Prototype).js
@@ -126,8 +126,130 @@ the constructor without arguments anymore.
 Otherwise, this code behaves exactly the same as the previous code. The prototype
 chain remains intact, so all instances of Square inherit from Rectangle.prototype
 and the constructor is restored in the same step
+
+Note: Always make sure that you overwrite the prototype before adding propertoes
+to it, or you will lose the added methods when the overwrite happens
 */
 
+console.log("\n\t• Constructor Stealing");
 
+/*
+Because inheritance is accomplished through prototype chains in JS, you don't
+need to call an object's supertype constructor. If you do want to call the
+supertype constructor from the subtype constructor, then you need to take advantage
+of how JavaScript work.
+We already learned about the call() and apply() methods, which allow functions
+to be called with a different this value. That's exactly how constructor stealing
+works. You simply call the supertype constructor from the subtype constructor using
+either call() or apply() to pass in the newly created object. In effect, you're
+stealing the supertype constructor for your own object, as in this example:
+*/
+
+function Rectangle(length, width){
+	this.length = length;
+	this.width = width;
+}
+
+Rectangle.prototype.getArea = function(){
+	return this.length * this.width;
+};
+
+Rectangle.prototype.toString = function(){
+	return "[Rectangle " + this.length + "x" + this.width + "]";
+};
+
+//inherits from rectangle
+function Square(size){
+	Rectangle.call(this, size, size);
+
+	//optional: add new properties or override existing ones here
+}
+
+Square.prototype = Object.create(Rectangle.prototype, {
+	constructor: {
+		configurable: true,
+		enumerable: true,
+		value: Square,
+		writable: true
+	}
+});
+
+Square.prototype.toString = function(){
+	return "[Square " + this.length + "x" + this.width + "]";
+};
+
+var square = new Square(6);
+
+console.log(square.length); // -> 6
+console.log(square.width); // -> 6
+console.log(square.getArea()) // -> 36
+
+/*
+See what happpened here? the Square constructor calls the Rectangle constructor
+passes in this as well as size two times.
+Doing so creates the lenght and width properties on the new object and makes each
+equal to size. This is the way to avoid redefining properties from a constructor
+from which you want to inherit.
+You can add new or change existing properties after calling the super type constructor.
+
+This two-step process is useful then you need to accomplish inheritance between
+custom types. You'll always need to call the supertype constructor from within
+the subtype constructor. Generally, you'll modify the prototype for method
+inheritance  and use constructor stealing for properties. This approach is
+typically referred to as pseudoclassical inheritance because it mimics classical
+inheritnce from class-based languages.
+*/
+
+console.log("\n\t• Accessing Supertype Methods");
+
+
+/*
+In the previous example, the Square type has its own toString() method that
+shadows toString() on the prototype. It is fairly common to ovoerride supertype
+methods with new functionality in the subtype, but what if you still want to
+access the supertype method? In other languages, you might be able to say
+super.toString(), but JavaScript doen't have anything similar.
+Instead, you can directly access the method on the supertype's prototype
+and use either call() or apply() to execute the method on the subtype object.
+*/
+
+function Rectangle(length, width){
+	this.length = length;
+	this.width = width;
+}
+
+Rectangle.prototype.getArea = function(){
+	return this.length * this.width;
+};
+
+Rectangle.prototype.toString = function(){
+	return "[Rectangle " + this.length + "x" + this.width + "]";
+};
+
+function Square(size){
+	Rectangle.call(this, size, size);
+}
+
+Square.prototype = Object.create(Rectangle.prototype, {
+	constructor: {
+		configurable: true,
+		enumerable: true,
+		value: true,
+		writable: true
+	}
+});
+
+//call the supertype method
+Square.prototype.toString = function(){
+	var text = Rectangle.prototype.toString.call(this);
+	return text.replace("Rectangle", "Square");
+};
+
+console.log(square.length); // -> 6
+console.log(square.width); // -> 6
+console.log(square.getArea()) // -> 36
+console.log(square.toString()) // -> [Square 6x6]
+
+//Done.
 
 
